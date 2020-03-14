d60cca370b4ab7aa6353a9da58c6f68e6cae0ae8
Marisa Clardy <marisa@clardy.eu>
2020-03-14 14:07:53 -0500

    Adjust random to be floating point random in flight factory.

--- database/factories/FlightFactory.php
+++ database/factories/FlightFactory.php
@@ -6,11 +6,13 @@ use App\Flight;
 use Faker\Generator as Faker;
 
 $factory->define(Flight::class, function (Faker $faker) {
+    $rand = fn(float $min, float $max): float => round(($min + (mt_rand() / mt_getrandmax()) * abs($max-$min)), 6);
+
     return [
         'flight_time' => now(),
-        'lat' => rand(-90, 90),
-        'long' => rand(-90, 90),
-        'duration_in_seconds' => rand(0, 900),
+        'lat' => $rand(-90, 90),
+        'long' => $rand(-90, 90),
+        'duration_in_seconds' => mt_rand(0, 900),
         'notes' => $faker->sentence,
     ];
 });
